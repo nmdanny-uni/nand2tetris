6b982459a9d3bd35ae0237f65767fa56bfe4246d
Daniel Kerbel <nmdanny@gmail.com>
2019-08-26 20:13:01 +0300

    Finish Fill program

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,73 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
+
 // Put your code here.
+
+// initialize pressed. 0 means white, -1 means black
+@pressed
+M = 0
+
+// here we set the END_OF_SCREEN constant, equals SCREEN + 256 rows * 32 words = 24576
+// we end one unit before, since
+@24576
+D = A
+@END_OF_SCREEN
+M = D
+
+// main program loop
+(infiniteLoop)
+
+// inspecting the pressed key
+@KBD
+D = M
+@setPressed
+D; JNE // if scan_code != 0, call setPressed
+@pressed
+M = 0  // otherwise, pressed = 0
+(setScreenColor)
+// setting screen color
+
+// initialize initial screen index(loop variable)
+@SCREEN
+D = A
+@screenPos
+M = D
+
+// while (screenPos != END_OF_SCREEN)
+(screenLoop)
+@screenPos
+D = M
+@END_OF_SCREEN
+D = D - M
+@infiniteLoop
+D; JEQ
+
+// set the entire pixel word as required (Screen[screenPos] = pressed)
+// remembering that pressed is 0(black) if unpressed, or -1 (white) if pressed
+@pressed
+D = M
+@screenPos
+A = M
+M = D
+
+// advance screenPos by 32
+@1
+D = A
+@screenPos
+M = M + 1
+
+// continue with the screenPos loop
+@screenLoop
+0; JMP
+
+// continue with our input loop
+@infiniteLoop
+0; JMP
+
+// pressed = 1
+(setPressed)
+@pressed
+M=-1  // -1 is 1111...11 in bits, allowing us to color 16 bits at once
+@setScreenColor
+0; JMP
