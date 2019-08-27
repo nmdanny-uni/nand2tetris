78d46bfbd45b36afa17e85eb437d50fc1d114956
Daniel Kerbel <nmdanny@gmail.com>
2019-08-27 14:15:23 +0300

    Fix indentation, begin loop from 14 instead of 15 (Divide)

--- projects/04/divide/Divide.asm
+++ projects/04/divide/Divide.asm
@@ -1,67 +1,76 @@
-  // Algorithm that performs R13/R14 = R15 (integer division)
-  // Where the inputs are positive.
+// Algorithm that performs R13/R14 = R15 (integer division)
+// Where the inputs are positive.
 
-  // We'll use a long division method, based on
-  // https://en.wikipedia.org/wiki/Division_algorithm#Integer_division_(unsigned)_with_remainder
-  // Here's C code:
-  //
-  //  unsigned short divide(unsigned short n, unsigned short d){
-  //   unsigned short q=0;
-  //   unsigned short r=0;
-  //   for (short i=15; i>=0; i--){
-  //   r = r << 1;
-  //   // we'll set the 1st bit of r, to the i'th bit of
-  //   // n, in the following way:
-  //   // first, zero the 1st bit of r
-  //   r = r & ~(1);
-  //   // then, if the i'th bit of n exists, 
-  //   if (n & (1 << i)) {
-  //   r = r | 1; // we set again the 1st bit of r.
-  // }
-  // if (r >= d){
-  // r = r - d;
-  // q = q | (1 << i);
-  // }
-  // }
-  // return q;
-  // }
+// We'll use a long division method, based on
+// https://en.wikipedia.org/wiki/Division_algorithm#Integer_division_(unsigned)_with_remainder
+// Here's C code:
+//
+// unsigned short divide(unsigned short n, unsigned short d){
+//   unsigned short q=0;
+//   unsigned short r=0;
+//   for (short i=15; i>=0; i--){
+//     r = r << 1;
+//     // we'll set the 1st bit of r, to the i'th bit of
+//     // n, in the following way:
+//     // first, zero the 1st bit of r
+//     r = r & ~(1);
+//     // then, if the i'th bit of n exists, 
+//     if (n & (1 << i)) {
+//       r = r | 1; // we set again the 1st bit of r.
+//     }
+//     if (r >= d){
+//       r = r - d;
+//       q = q | (1 << i);
+//     }
+//   }
+//   return q;
+// }
 
-  // The translation of this to asm is mostly straightforward, except that
-  // for calculating (1 << i), since our shift operator doesn't take any parameters
-  // we'll first calculate 1 << 15 (this is 2^15), and every iteration we'll right
-  // shift it.
+// The translation of this to asm is mostly straightforward, except that
+// for calculating (1 << i), since our shift operator doesn't take any parameters
+// we'll first calculate 1 << 15 (this is 2^15), and every iteration we'll right
+// shift it.
 
+// However, there's another issue - this C code assumes unsigned integers, but
+// in hack, all integers are signed, even though we assume that R13,R14 are positive
+// Thus, beginning the algorithm from the 15th bit will cause a 'wrong' result
+// ('wrong' - technically, if we interpret the output bits as an unsigned number,
+//  it will be right. but the CPU emulator will show a negative number, since in
+//  hack, numbers are signed by default)
 
-  // ======================= Initialization of variables =========================
-  // For readability, I use custom symbols instead of R13/R14/R15, but at the end
-  // of the program I update R15 to become the quotinent.
+// Therefore, we'll begin the loop from the 14th bit going down - this way,
+// we won't affect the sign of the algorithm. (and our shift pattern will be 1 << 14,
+// that is, 2^14)
 
-  @R13
-  D = M
-  @n
-  M = D // dividend
+// ======================= Initialization of variables =========================
+// For readability, I use custom symbols instead of R13/R14/R15, but at the end
+// of the program I update R15 to become the quotinent.
 
-  @R14
-  D = M
-  @d
-  M = D // divisor
+@R13
+D = M
+@n
+M = D // dividend
 
-  @q
-  M = 0 // quotinent
+@R14
+D = M
+@d
+M = D // divisor
 
-  @r
-  M = 0 // remainder
+@q
+M = 0 // quotinent
 
-  @15
-  D = A
-  @i
-  M = D // index of bit being operated on, beginning with the MSB(15)
+@r
+M = 0 // remainder
 
-  @16384
-  D = A
-  @shiftPattern
-  M = D // 2^14, this allows us to access the 15th bit(we ignore the 16th since numbers
-        // are signed)
+@14
+D = A
+@i
+M = D // index of bit being operated on, beginning with one digit before the MSB(the 14th)
+
+@16384
+D = A
+@shiftPattern
+M = D // 2^14, this allows us to access the 14th bit
 
 
 
