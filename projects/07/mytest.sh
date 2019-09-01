1c89a836ce665a973943a3a288f12786f289764f
Daniel Kerbel <nmdanny@gmail.com>
2019-09-02 00:19:46 +0300

    Organize tests, include test files from ex8

--- projects/07/mytest.sh
+++ projects/07/mytest.sh
@@ -5,7 +5,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 WORK_DIR=$DIR/test_dir
 rm -rf $WORK_DIR
 mkdir $WORK_DIR
-find . -type f -regex ".*/.*\.\(vm\|tst\|cmp\)" -exec cp {} $WORK_DIR/ \;
+find $DIR/test_src -type f -regex ".*/.*\.\(vm\|tst\|cmp\)" -exec cp {} $WORK_DIR/ \;
+
+rm -rf $WORK_DIR/programs
+mkdir $WORK_DIR/programs
+cp -r $DIR/test_programs_dir/* $WORK_DIR/programs
 
 SUCC=0
 FAIL=0
@@ -13,7 +17,7 @@ FAIL=0
 for i in $WORK_DIR/*.vm;
 do
 	name=`basename $i .vm`
-  echo "Translating $i"
+  echo "Testing .vm file $i"
 	$DIR/VMtranslator $i $*
 	execOut=$(CPUEmulator.sh $WORK_DIR/$name.tst)
 
@@ -24,6 +28,26 @@ do
       SUCC=$((SUCC+1))
   fi;
 
+done;
+
+for i in $WORK_DIR/programs/*;
+do
+    echo "Testing vm program at directory $i"
+    $DIR/VMtranslator $i $* -b
+    for tst in $i/*.tst;
+    do
+        if [[ $tst =~ "VME" ]]
+        then
+            continue
+        fi;
+        execOut=$(CPUEmulator.sh $tst $*)
+        if [[ $execOut == "" ]]  # on fail, no success msg is printed to stdout
+        then
+            FAIL=$((FAIL+1))
+        else
+            SUCC=$((SUCC+1))
+        fi;
+    done;
 done;
 
 echo "Success count: $SUCC"
