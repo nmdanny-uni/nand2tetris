8ddca20fa9e88d95d4d2cbb3befd9d621397d848
nmdanny <nmdanny@gmail.com>
2019-10-29 21:28:02 +0200

    Fix tiny "test" and refactor printing fun

--- projects/10/tests/ast_test.py
+++ projects/10/tests/ast_test.py
@@ -16,11 +16,13 @@ def string_to_semantic(s: str,
         engine = CompilationEngine(file.name)
         return parse_fun(engine)
 
+def print_semantic(obj: Semantic):
+    print("\n"+json.dumps(asdict(obj), indent=4))
+
 def test_can_handle_ast():
     print()
-    expr = string_to_semantic("1 * 2 + 3 / (4 * 5) + arr[5]",
+    expr = string_to_semantic("1 * 2 + 3 / (4 * 5) + arr[5] + -1 & ~5",
                               CompilationEngine.parse_expression)
-    ast = Expression.from_expression(expr)
-    print("\n"+json.dumps(asdict(ast), indent=4))
-    for x in ast.iter_postorder_dfs():
+    print_semantic(expr)
+    for x in expr.iter_postorder_dfs():
         print(x)
